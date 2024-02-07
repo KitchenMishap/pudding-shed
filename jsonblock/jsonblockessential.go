77b53c9dd8824b1558025bc091a23c80e98955ca
NigelGSharpUK <nigel@nsharp.uk>
2024-02-07 21:24:38 +0000

    Include non-essential ints in the json types

--- jsonblock/jsonblockessential.go
+++ jsonblock/jsonblockessential.go
@@ -8,6 +8,7 @@ import (
 // In here are partial specifications of the JSON format for a block (and contained transactions etc) coming from Bitcoin core REST API.
 // They are partial as we're not interested in everything that's in the JSON.
 // Hence the word "essential".
+// Some non-essential fields are included; these are the integers which we can conveniently handle in a unified way
 
 // SEE ALSO transaction.go, where these json types are furnished with functions to implement various interfaces
 
@@ -16,6 +17,16 @@ type JsonBlockEssential struct {
 	J_hash   string               `json:"hash"`
 	J_tx     []jsonTransEssential `json:"tx"`
 	hash     indexedhashes.Sha256 `json:"-"` // Does not appear in json. Calculated after parsing of whole block
+
+	// Non essential integers
+	J_version      int64 `json:"version"`
+	J_time         int64 `json:"time"`
+	J_mediantime   int64 `json:"mediantime"`
+	J_nonce        int64 `json:"nonce"`
+	J_difficulty   int64 `json:"difficulty"`
+	J_strippedsize int64 `json:"strippedsize"`
+	J_size         int64 `json:"size"`
+	J_weight       int64 `json:"weight"`
 }
 
 type jsonTransEssential struct {
@@ -24,6 +35,13 @@ type jsonTransEssential struct {
 	J_vout []jsonTxoEssential   `json:"vout"`
 	txid   indexedhashes.Sha256 `json:"-"` // Does not appear in json. Calculated after parsing of whole block
 	handle TransHandle          `json:"-"` // Does not appear in json. "calculated" after parsing of whole block
+
+	// Non essential integers
+	J_version  int64 `json:"version"`
+	J_size     int64 `json:"size"`
+	J_vsize    int64 `json:"vsize"`
+	J_weight   int64 `json:"weight"`
+	J_locktime int64 `json:"locktime"`
 }
 
 type jsonTxiEssential struct {
