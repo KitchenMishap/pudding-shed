7a986a2f7a1fc60c71c607466c3e1d858f1a572a
NigelGSharpUK <nigel@nsharp.uk>
2024-09-19 05:25:35 +0100

    Oops

--- concurrency/sequencercontainer.go
+++ concurrency/sequencercontainer.go
@@ -9,36 +9,35 @@ type Sequenceable interface {
 // Objects are sent in to SequenceContainer.InChan in a pseudo-sequence.
 // Objects come out of outChan in strict sequence order.
 // The intention is to place this in a pipeline after a thread pool which may have disrupted the original sequence.
-func NewSequencerContainer(firstOut int64, bloatedCount int64, outChan* chan Sequenceable) *SequencerContainer {
+func NewSequencerContainer(firstOut int64, bloatedCount int64, outChan *chan Sequenceable) *SequencerContainer {
 	result := SequencerContainer{}
-	result.InChan = make( chan Sequenceable )
+	result.InChan = make(chan Sequenceable)
 	result.outChan = outChan
-	result.theMap = make map[int64]Sequenceable
+	result.theMap = make(map[int64]Sequenceable)
 	result.nextOut = firstOut
 	result.bloatedCount = bloatedCount
-	result.currentlyBloated = false;
+	result.currentlyBloated = false
 	go result.worker()
 	return &result
 }
 
 type SequencerContainer struct {
-	InChan chan Sequenceable
-	outChan* chan Sequenceable
-	theMap map[int64]Sequenceable
-	nextOut int64
-	bloatedCount int64
+	InChan           chan Sequenceable
+	outChan          *chan Sequenceable
+	theMap           map[int64]Sequenceable
+	nextOut          int64
+	bloatedCount     int64
 	currentlyBloated bool
 }
 
-func (sc *SequencerContainer) worker()
-{
+func (sc *SequencerContainer) worker() {
 	newVal := <-container.InChan
 	if newVal == nil {
 		// input channel closed, should be able to empty the map
 		for sendNextOut() {
 		}
 		if len(sc.theMap) > 0 {
-			panic( "The sequencerContainer became jammed")
+			panic("The sequencerContainer became jammed")
 		}
 		sc.outChan.Close()
 		return
@@ -61,10 +60,10 @@ func (sc *SequencerContainer) sendNextOut() bool {
 	if !ok {
 		return false
 	} else {
-		sc.outChan<- next
+		sc.outChan <- next
 		sc.nextOut++
 		delete(sc.theMap, sc.nextOut)
 		sc.currentlyBloated = int64(len(sc.theMap)) >= sc.bloatedCount
-		return true;
+		return true
 	}
 }
