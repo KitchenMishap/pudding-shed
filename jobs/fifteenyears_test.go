44fdbb8078a8a54db05f205806814e53c0428943
NigelGSharpUK <130097204+KitchenMishap@users.noreply.github.com>
2024-09-24 06:29:43 +0100

    Test to isolate memory leak problem

--- jobs/fifteenyears_test.go
+++ jobs/fifteenyears_test.go
@@ -1,6 +1,11 @@
 package jobs
 
-import "testing"
+import (
+	"fmt"
+	"github.com/KitchenMishap/pudding-shed/indexedhashes"
+	"github.com/KitchenMishap/pudding-shed/tinychain"
+	"testing"
+)
 
 func TestFiveYearsDelegated(t *testing.T) {
 	SeveralYearsParallel(4, "delegated")
@@ -8,4 +13,26 @@ func TestFiveYearsDelegated(t *testing.T) {
 
 func TestOneYearUndelegated(t *testing.T) {
 	SeveralYearsPrimaries(1, "separate files")
+}
+
+func TestMemoryLeak(t *testing.T) {
+	cr := indexedhashes.NewUniformHashStoreCreator(1000000000, "F:\\Data\\TestMemLeak", "TestName", 2)
+	err := cr.CreateHashStore()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	hs, err := cr.OpenHashStore()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	for i := uint64(0); i < 10000000; i++ {
+		hash := tinychain.HashOfInt(i)
+		_, err := hs.AppendHash(&hash)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
 }
