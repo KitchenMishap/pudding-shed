54665c88066299f8ee66ec86a80a4c12954bfc8a
NigelGSharpUK <130097204+KitchenMishap@users.noreply.github.com>
2025-01-11 13:31:51 +0000

    indexedhashes3: sensible params for 16 years of block hashes (with a test to show how we got there)

--- indexedhashes3/sensibleparams.go
+++ indexedhashes3/sensibleparams.go
@@ -1,15 +1,18 @@
 package indexedhashes3
 
-// Some sensible bitsPerHashIndex for 15 or 16 years
+// Some sensible bitsPerHashIndex for 15 or 16 years, and for 2 years (for testing)
+
+// --------------------------------------------------------------------------
+// 15 or 16 years
 //
 //	Count at 15 years:
 const bitsFor2milBlocks = int64(21) // 2^21 = 2,097,152 blocks				There were 824,204 blocks
 const bitsFor2bilTrans = int64(31)  // 2^31 = 2,147,483,648 transactions	There were 947,337,057 transactions
 const bitsFor4bilAddrs = int64(32)  // 2^32 = 4,294,967,296 addresses		There must be fewer addresses than txos
-//																			(There were 2,652,374,369 txos, including spent)
-
+// (There were 2,652,374,369 txos, including spent)
 const addressesEstimate16Years = int64(3000000000)
 const transactionsEstimate16Years = int64(1000000000)
+const blocksEstimate16Years = int64(1000000)
 
 func Sensible16YearAddressHashParams() *HashIndexingParams {
 	// Run test Test16YearsAddressParams() to see how these numbers are arrived at
@@ -43,5 +46,23 @@ func Sensible16YearsTransactionHashParams() *HashIndexingParams {
 		9898557,                     // numberOfBins (result of some optimization calculations)
 		132,                         // entriesInBinStart (result of some optimization calculations)
 		31)                          // bytesPerBinEntry (result of some optimization calculations)
-
 }
+
+func Sensible16YearsBlockHashParams() *HashIndexingParams {
+	// Run test Test16YearsBlockParams() to see how these numbers are arrived at
+	// The following values give:
+	// BytesPerBinStart = 1015 (didn't get anywhere near 4096)
+	// OverflowFilesEstimate = 238 (good, a nice balance as a million or so files take a very long time to copy)
+	// 1% of bins resorting to an overflow file
+	// Estimated hash store size on disk = 0GB (good, insignificant)
+	return NewHashStoreParams(
+		bitsFor2bilTrans,            // bitsPerHashIndex
+		transactionsEstimate16Years, // hashCountEstimate
+		2,                           // digitsPerNumberedFolder
+		42926,                       // numberOfBins (result of some optimization calculations)
+		35,                          // entriesInBinStart (result of some optimization calculations)
+		29)                          // bytesPerBinEntry (result of some optimization calculations)
+}
+
+// 2 years (for testing)
+// ToDo
--- indexedhashes3/sensibleparams_test.go
+++ indexedhashes3/sensibleparams_test.go
@@ -6,5 +6,10 @@ func Test16YearsAddressParams(t *testing.T) {
 	graphGigabytes(bitsFor4bilAddrs, addressesEstimate16Years, 4096, 4096)
 }
 func Test16YearsTransactionParams(t *testing.T) {
+	// We don't get any 4096 byte results, so we use 4092 (132 * 31 bytes)
 	graphGigabytes(bitsFor2bilTrans, transactionsEstimate16Years, 4092, 4096)
+}
+func Test16YearsBlockParams(t *testing.T) {
+	// We don't get anywhere near 4096 byte results, so we use 1015 (35 * 29 bytes)
+	graphGigabytes(bitsFor2milBlocks, blocksEstimate16Years, 1000, 1024)
 }
