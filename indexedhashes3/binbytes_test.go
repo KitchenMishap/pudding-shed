e3e958f52f52ef47260f21b045c63a5a476a8998
NigelGSharpUK <130097204+KitchenMishap@users.noreply.github.com>
2025-01-12 11:15:28 +0000

    A test that fails! insertBinEntry then lookupByHash fails

---
+++ indexedhashes3/binbytes_test.go
@@ -0,0 +1,33 @@
+package indexedhashes3
+
+import "testing"
+
+func TestInsert(t *testing.T) {
+	b := newEmptyBin()
+	if len(b) != 0 {
+		t.Error("new bin not empty")
+	}
+
+	tr1 := [24]byte{0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+		0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77}
+	trBytes := tr1
+	tr := truncatedHash(trBytes)
+	hi := hashIndex(12345)
+	sn := sortNum(23456)
+	params := Sensible2YearsBlockHashParams()
+
+	b.insertBinEntry(sn, hi, &tr, params)
+	if len(b) != 1 {
+		t.Error("inserted bin, size not 1")
+	}
+
+	ind := b.findIndexBasedOnSortNum(sn, params)
+	if ind != 0 {
+		t.Error("inserted bin, findIndexBasedOnSortNum not 0")
+	}
+
+	n := b.lookupByHash(&tr, sn, params)
+	if n != 0 {
+		t.Error("inserted bin, lookup not 0")
+	}
+}
