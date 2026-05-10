e9fb8d623aac614e18264978bee2d2c92b0d0e48
Kitchen Mishap <hello@kitchenmishap.art>
2026-05-10 06:48:02 +0100

    Get main_test.go working again

--- main_test.go
+++ main_test.go
@@ -2,27 +2,40 @@ package main
 
 import (
 	"fmt"
-	"github.com/KitchenMishap/pudding-shed/chainstorage"
-	"github.com/KitchenMishap/pudding-shed/jobs"
+	"strconv"
 	"testing"
 	"time"
+
+	"github.com/KitchenMishap/pudding-shed/chainstorage"
+	"github.com/KitchenMishap/pudding-shed/jobs"
 )
 
 // We have a TestMain which mimics Main()... because you can only CPU profile a test!
-func TestMain(m *testing.M) {
-	t := time.Now()
-	fmt.Println(t.Format("Mon Jan 2 15:04:05"))
+func Test_Main(t *testing.T) {
+	tt := time.Now()
+	fmt.Println(tt.Format("Mon Jan 2 15:04:05"))
+
+	path := "E:\\Data\\TestMain"
+	gb := 16
+	threads := 10
+	years := 2
+	fmt.Println("Dir=" + path)
+	fmt.Println("Yrs=" + strconv.Itoa(years))
+	fmt.Println("Gb=" + strconv.Itoa(gb))
+	fmt.Println("Threads=" + strconv.Itoa(threads))
 
 	chainstorage.PrevFirstTxo = -1
 	chainstorage.PrevTrans = -1
 
-	err := jobs.SeveralYearsPrimaries(16, "delegated", true, true, true)
+	err := jobs.SeveralYearsPrimaries(years, "delegated", true, true, true,
+		path, gb, threads)
 
 	if err != nil {
 		println(err.Error())
 		println("There was an error :-O")
+		t.Error(err)
 	}
 	fmt.Println("End of TestMain()")
-	t = time.Now()
-	fmt.Println(t.Format("Mon Jan 2 15:04:05"))
+	tt = time.Now()
+	fmt.Println(tt.Format("Mon Jan 2 15:04:05"))
 }
